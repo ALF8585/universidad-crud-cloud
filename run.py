9f2ad8636cbf65363dd1b0617fb8f0b2913fd524
ALF8585 <139729335+ALF8585@users.noreply.github.com>
2025-07-03 12:14:45 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
-from app import create_app, db
-from app.models import Estudiante
+from flask import Flask, render_template
+from models import Universidad, Carrera
 
-app = create_app()
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=10000)
+# Aquí debajo
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/universidades')
+def listar_universidades():
+    universidades = Universidad.query.all()
+    return render_template('universidades.html', universidades=universidades)
+
+@app.route('/carreras')
+def listar_carreras():
+    carreras = Carrera.query.all()
+    return render_template('carreras.html', carreras=carreras)
