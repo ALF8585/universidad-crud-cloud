8714ef369bafb9cc26c2bb36ea2711c0b5e0f645
ALF8585 <139729335+ALF8585@users.noreply.github.com>
2025-07-03 12:23:55 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template
-from models import Universidad, Carrera
+from app.models import Universidad, Carrera
 
 app = Flask(__name__)
 
-# Aquí debajo
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,6 +16,6 @@ def listar_universidades():
 def listar_carreras():
     carreras = Carrera.query.all()
     return render_template('carreras.html', carreras=carreras)
+
 if __name__ == '__main__':
     app.run(debug=True)
-
