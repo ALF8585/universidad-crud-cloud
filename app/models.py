5879e04c1dd51fc4aafb640d3d694ba8506c1149
ALF8585 <139729335+ALF8585@users.noreply.github.com>
2025-07-03 12:29:54 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,15 @@
-from app.models import Universidad, Carrera
-from app.routes import app
+# app/models.py
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
-class Estudiante(db.Model):
+class Universidad(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
+    direccion = db.Column(db.String(200), nullable=False)
+
+class Carrera(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    universidad_id = db.Column(db.Integer, db.ForeignKey('universidad.id'), nullable=False)
+    universidad = db.relationship('Universidad', backref=db.backref('carreras', lazy=True))
